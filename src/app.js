52c7d8c5f409fd1bc6ab9ba5f89993c54842df98
hugoherculano <hugohercullano@gmail.com>
2023-10-06 02:08:17 -0300

    feat: routes created for tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,115 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  try {
+    return response.status(200).json(repositories);
+  } catch {
+    return response.status(400).json({ message: 'Error in repository listing! :/' });
+  }
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  if(!title || !url || !techs) {
+    return response.status(400).json({ message: 'Creation data is incomplete!'});
+  }
+
+  try {
+
+    const repository = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes: 0
+    }
+
+    repositories.push(repository);
+
+    return response.status(201).json(repository);
+  } catch {
+    return response.status(400).json({ message: 'Error in creation! :/' });
+  }  
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  try {
+    const repository = repositories.find(item => {
+      return id === item.id;
+    });
+
+    if(!repository) return response.status(400).json({ message: 'Repository not found!'});
+
+    const newRepository = {
+      ...repository,
+      title,
+      url,
+      techs,
+    }
+
+    const repositoryIndex = repositories.findIndex((item) => item.id === id);
+
+    repositories[repositoryIndex] = newRepository;
+
+    return response.status(200).json(newRepository);
+
+  } catch {
+    return response.status(400).json({ message: 'Error in update! :/' });
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  try {
+    const indexRepository = repositories.findIndex(item => {
+      return item.id === id;
+    });
+
+    if(indexRepository === -1) {
+      return response.status(400).json({ message: 'Repository not found!'});
+    }
+
+    repositories.splice(indexRepository, 1);
+
+    return response.status(204).json();
+
+  } catch {
+    return response.status(400).json({ message: 'Error in delete! :/' });
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  try {
+    const repository = repositories.find(item => {
+      return id === item.id;
+    });
+
+    if(!repository) return response.status(400).json({ message: 'Repository not found!'});
+
+    const amountLikes = repository.likes + 1;
+
+    const newRepository = {
+      ...repository,
+      likes: amountLikes
+    }
+
+    const repositoryIndex = repositories.findIndex((item) => item.id === id);
+
+    repositories[repositoryIndex] = newRepository;
+
+    return response.status(200).json(newRepository);
+
+  } catch {
+    return response.status(400).json({ message: 'Error in update likes! :/' });
+  }
 });
 
 module.exports = app;
